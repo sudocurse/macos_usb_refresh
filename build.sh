b119a96eaca09b27229cf5db79f89575b057d6ca
sudocurse <sudocurse@gmail.com>
2024-03-05 16:17:33 -0800

    cwd-agnostic

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+# cd to the directory of the script
+cd "$(dirname "$0")"
 cc -framework Foundation -framework IOKit reenumerate.m -o usb_refresh
 sudo cp ./usb_refresh /usr/local/bin/usb_refresh
